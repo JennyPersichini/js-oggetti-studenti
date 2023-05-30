3714e40a790432f162562306c9e3672ec30c2e22
JennyPersichini <nenny1096@gmail.com>
2023-05-30 12:28:23 +0200

    aggiunta array students e ciclo for

--- script.js
+++ script.js
@@ -13,7 +13,55 @@ console.log(student);
 
 for (let key in student) {
 
-    console.log(key);
-    console.log(student[key]);
+    console.log(key + ": " + student[key]);
 
 };
+
+//3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte).
+
+let students = [
+
+    {
+        name: "Pinco",
+        surname: "Pallino",
+        age: 19,
+        peso: 78
+    },
+
+    {
+        name: "Sara",
+        surname: "Verdi",
+        age: 25,
+        peso: 75
+    },
+
+    {
+        name: "Michela",
+        surname: "Bianchi",
+        age: 30,
+        peso: 81
+    },
+
+    {
+        name: "Jimmy",
+        surname: "Patato",
+        age: 18,
+        peso: 68
+    },
+    
+    {
+        name: "Sharon",
+        surname: "Azzurro",
+        age: 28,
+        peso: 59
+    },
+]
+
+console.log(students);
+
+//4. Ciclare su tutti gli studenti e stampare per ognuno di essi: nome e cognome.
+
+for (let i = 0; i < students.length; i++) {
+    console.log("Nome: " + students[i].name);
+    console.log("Cognome: " + students[i].surname);
+  } 
